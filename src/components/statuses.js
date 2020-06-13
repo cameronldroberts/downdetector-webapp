f549369eecfde0f2237382d5eb7df178c0a70dee
CAMERON ROBERTS <cameronldroberts@gmail.com>
2020-06-13 19:59:46 +0100

    show the different

--- src/components/statuses.js
+++ src/components/statuses.js
@@ -1,20 +1,61 @@
+// import React from 'react'
+
+// // import  from 'react-bootstrap/Card'
+// // import {Card, CardGroup } from 'react-bootstrap'
+// import { Card , Image, Icon } from 'semantic-ui-react'
+// const GitHub = require('../resources/GitHub.svg');
+// const Twitter = require('../resources/Twitter.svg')
+
+// const Statuses = ({statuses}) => {
+//     return (
+//         <div>
+//             <center><h1>Service Status</h1></center>
+//             <Card.Group className="card-group"> 
+//             {statuses.map((status) => (
+
+//             <Card>
+//             <Image src={Twitter} wrapped ui={false} />
+//             <Card.Content>
+//                 <Card.Header>{status.name}</Card.Header>
+//                 <Card.Description>
+//                 { status.status.description}
+//                 </Card.Description>
+//                 <Card.Description>
+//                 { status.status.indicator}
+//                 </Card.Description>
+                
+//             </Card.Content>
+//             <Card.Content extra>
+//                 <a>
+//                 <Icon name='question icon' />
+//                 More information
+//                 </a>
+//             </Card.Content>
+//             </Card>
+//             ))}
+//             </Card.Group>
+//         </div>
+//     )
+// };
+
+// export default Statuses
 import React from 'react'
 
 // import  from 'react-bootstrap/Card'
 // import {Card, CardGroup } from 'react-bootstrap'
-import { Card , Image } from 'semantic-ui-react'
-// const GitHub = require('../resources/GitHub.svg');
-// const Twitter = require('../resources/Twitter.svg')
+import { Card , Image, Icon } from 'semantic-ui-react'
+const GitHub = require('../resources/GitHub.svg');
+const Twitter = require('../resources/Twitter.svg')
 
 const Statuses = ({statuses}) => {
     return (
         <div>
             <center><h1>Service Status</h1></center>
-            <Card.Group className="card-group">
+
             {statuses.map((status) => (
 
             <Card>
-            <Image src="https://picsum.photos/216/216" wrapped ui={false} />
+            <Image src={Twitter} wrapped ui={false} />
             <Card.Content>
                 <Card.Header>{status.name}</Card.Header>
                 <Card.Description>
@@ -25,15 +66,14 @@ const Statuses = ({statuses}) => {
                 </Card.Description>
                 
             </Card.Content>
-            {/* <Card.Content extra>
+            <Card.Content extra>
                 <a>
-                <Icon name='user' />
-                22 Friends
+                <Icon name='question icon' />
+                More information
                 </a>
-            </Card.Content> */}
+            </Card.Content>
             </Card>
             ))}
-            </Card.Group>
         </div>
     )
 };
