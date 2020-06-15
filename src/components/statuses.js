9efa9039d47b36ed8fc2092143d8fc475770e200
CAMERON ROBERTS <cameronldroberts@gmail.com>
2020-06-15 18:47:57 +0100

    even more all

--- src/components/statuses.js
+++ src/components/statuses.js
@@ -1,3 +1,44 @@
+import React from 'react'
+
+// import  from 'react-bootstrap/Card'
+// import {Card, CardGroup } from 'react-bootstrap'
+import { Card , Image, Icon } from 'semantic-ui-react'
+const GitHub = require('../resources/GitHub.svg');
+const Twitter = require('../resources/Twitter.svg')
+
+const Statuses = ({statuses}) => {
+    return (
+        <div>
+            <center><h1>Service Status</h1></center>
+            <Card.Group className="card-group"> 
+            {statuses.map((status) => (
+
+            <Card>
+            <Image src={GitHub} wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>{status.name}</Card.Header>
+                <Card.Description>
+                { status.status.description}
+                </Card.Description>
+                <Card.Description>
+                { status.status.indicator}
+                </Card.Description>
+                
+            </Card.Content>
+            <Card.Content extra>
+                <a>
+                <Icon name='question icon' />
+                More information
+                </a>
+            </Card.Content>
+            </Card>
+            ))}
+            </Card.Group>
+        </div>
+    )
+};
+
+export default Statuses
 // import React from 'react'
 
 // // import  from 'react-bootstrap/Card'
@@ -10,7 +51,7 @@
 //     return (
 //         <div>
 //             <center><h1>Service Status</h1></center>
-//             <Card.Group className="card-group"> 
+
 //             {statuses.map((status) => (
 
 //             <Card>
@@ -33,49 +74,8 @@
 //             </Card.Content>
 //             </Card>
 //             ))}
-//             </Card.Group>
 //         </div>
 //     )
 // };
 
 // export default Statuses
-import React from 'react'
-
-// import  from 'react-bootstrap/Card'
-// import {Card, CardGroup } from 'react-bootstrap'
-import { Card , Image, Icon } from 'semantic-ui-react'
-const GitHub = require('../resources/GitHub.svg');
-const Twitter = require('../resources/Twitter.svg')
-
-const Statuses = ({statuses}) => {
-    return (
-        <div>
-            <center><h1>Service Status</h1></center>
-
-            {statuses.map((status) => (
-
-            <Card>
-            <Image src={Twitter} wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>{status.name}</Card.Header>
-                <Card.Description>
-                { status.status.description}
-                </Card.Description>
-                <Card.Description>
-                { status.status.indicator}
-                </Card.Description>
-                
-            </Card.Content>
-            <Card.Content extra>
-                <a>
-                <Icon name='question icon' />
-                More information
-                </a>
-            </Card.Content>
-            </Card>
-            ))}
-        </div>
-    )
-};
-
-export default Statuses
