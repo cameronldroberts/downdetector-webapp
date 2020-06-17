f78296e531f8e5dd2bc494b54a8d853608a0ea58
CAMERON ROBERTS <cameronldroberts@gmail.com>
2020-06-17 22:37:44 +0100

    .

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Statuses from './components/statuses';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/navbar';
+// import Navbar from './components/navbar';
 class App extends Component {
     // render(){
     //     return (
--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-export const NavigationBar = () => (
-<Navbar bg="dark" variant="dark">
-<Navbar.Brand href="#home">
-  <img
-    alt=""
-    src="/logo.svg"
-    width="30"
-    height="30"
-    className="d-inline-block align-top"
-  />{' '}
-  React Bootstrap
-</Navbar.Brand>
-</Navbar>
-)
-export default Navbar
+// import React from 'react'
+// import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+// export const NavigationBar = () => (
+// <Navbar bg="dark" variant="dark">
+// <Navbar.Brand href="#home">
+//   <img
+//     alt=""
+//     src="/logo.svg"
+//     width="30"
+//     height="30"
+//     className="d-inline-block align-top"
+//   />{' '}
+//   React Bootstrap
+// </Navbar.Brand>
+// </Navbar>
+// )
+// export default Navbar
