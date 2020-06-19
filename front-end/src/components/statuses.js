969dab31cf4093e25d9e75ec37394a98084d0f8c
CAMERON ROBERTS <cameronldroberts@gmail.com>
2020-06-19 22:51:13 +0100

    nav

--- front-end/src/components/statuses.js
+++ front-end/src/components/statuses.js
@@ -2,37 +2,49 @@ import React from 'react'
 
 // import  from 'react-bootstrap/Card'
 // import {Card, CardGroup } from 'react-bootstrap'
-import { Card , Image } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 
-// const Twitter = require('../resources/Twitter.svg')
+import { Navbar } from 'react-bootstrap';
+import favicon from '../resources/favicon.ico'
 
-const Statuses = ({statuses}) => {
+const Statuses = ({ statuses }) => {
     return (
         <div>
-            <center><h1>Services Status</h1></center>
-            <Card.Group className="card-group">
-            {statuses.map((status) => (
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={favicon}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+            DownDetector
+            </Navbar.Brand>
+            </Navbar>
+            <Card.Group style={{ paddingTop: "2px" }} className="card-group">
+                {statuses.map((status) => (
 
-            <Card>
-            <Image src={require('../resources/'+status.name+'.svg')} wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>{status.name}</Card.Header>
-                <Card.Description>
-                { status.status.description}
-                </Card.Description>
-                <Card.Description>
-                { status.status.indicator}
-                </Card.Description>
-                
-            </Card.Content>
-            {/* <Card.Content extra>
+                    <Card>
+                        <Image src={require('../resources/' + status.name + '.svg')} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>{status.name}</Card.Header>
+                            <Card.Description>
+                                {status.status.description}
+                            </Card.Description>
+                            <Card.Description>
+                                {status.status.indicator}
+                            </Card.Description>
+
+                        </Card.Content>
+                        {/* <Card.Content extra>
                 <a>
                 <Icon name='user' />
                 22 Friends
                 </a>
             </Card.Content> */}
-            </Card>
-            ))}
+                    </Card>
+                ))}
             </Card.Group>
         </div>
     )
